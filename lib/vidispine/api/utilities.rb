4a1664354e747a70e80a1b5f984944c4f333f5ec
John Whitson <john.whitson@gmail.com>
2014-12-24 01:04:31 -0700

    Added item_create_with_proxy_using_ methods to the utilities class

--- lib/vidispine/api/utilities.rb
+++ lib/vidispine/api/utilities.rb
@@ -6,41 +6,89 @@ module Vidispine
 
     class Utilities < Client
 
-      def asset_create(args = { }, options = { })
-        original = args[:original]
-        lowres = args[:lowres]
+      # SEQUENCE THAT CREATE AN ITEM AND THE PROXY USING THE FILE ID
+      def item_create_with_proxy_using_file_id(args = { }, options = { })
+        # metadata = args[:metadata]
+        # metadata = metadata.map { |k,v| { 'name' => k, 'value' => v } } if metadata.is_a?(Hash)
+        #
+        # collection_id = args[:collection_id]
+        # unless collection_id
+        #   collection_name = args[:collection_name ]
+        #   unless collection_name
+        #     raise ArgumentError, 'collection_id or collection_name is required.'
+        #   end
+        #
+        #   collection = collection_get_by_name( { :collection_name => collection_name } )
+        #
+        #   unless collection
+        #     collection_create_if_not_exist = args[:collection_create_if_not_exist]
+        #     unless collection_create_if_not_exist
+        #       raise Argument, "collection not found. '#{collection_name}'"
+        #     end
+        #
+        #     # Create Collection
+        #     collection = collection_create(collection_name)
+        #   end
+        #
+        #   collection_id = collection['id']
+        # end
 
-        metadata = args[:metadata]
-        metadata = metadata.map { |k,v| { 'name' => k, 'value' => v } } if metadata.is_a?(Hash)
+        original_file_path = args[:original_file_path] || args[:original]
+        lowres_file_path = args[:lowres_file_path] || args[:lowres]
 
         placeholder_args = args[:placeholder_args] ||= { :container => 1, :video => 1 }
 
-        collection_id = args[:collection_id]
-        unless collection_id
-          collection_name = args[:collection_name ]
-          unless collection_name
-            raise ArgumentError, 'collection_id or collection_name is required.'
-          end
-
-          collection = collection_get_by_name( { :collection_name => collection_name } )
-
-          unless collection
-            collection_create_if_not_exist = args[:collection_create_if_not_exist]
-            unless collection_create_if_not_exist
-              raise Argument, "collection not found. '#{collection_name}'"
-            end
-
-            # Create Collection
-            collection = collection_create(collection_name)
-          end
-
-          collection_id = collection['id']
-        end
-
+        storage_id = args[:storage_id]
 
         # Create a placeholder
+        # /API/import/placeholder/?container=1&video=1
         place_holder = placeholder_create(placeholder_args)
+        item_id = place_holder['id']
 
+        # /API/storage/VX-2/file/?path=storages/test/test_orginal2.mp4
+        original_file = storage_file_get(:storage_id => storage_id, :path => original_file_path)
+        original_file_id = original_file['file'].first['id']
+
+        # /API/item/VX-98/shape?tag=original&fileId=[FileIDofOriginal]
+        item_shape_import(:item_id => item_id, :tag => 'original', :file_id => original_file_id)
+
+        # /API/storage/VX-2/file/?path=storages/test/test_proxy2.mp4
+        lowres_file = storage_file_get(:storage_id => storage_id, :path => lowres_file_path)
+        lowres_file_id = lowres_file['file'].first['id']
+
+        # /API/item/VX-98/shape?tag=lowres&fileId=[FileIDofProxy]
+        item_shape_import(:item_id => item_id, :tag => 'lowres', :file_id => lowres_file_id)
+
+        # /API/item/VX-98/thumbnail/?createThumbnails=true&createPoster
+        item_transcode(:item_id => item_id, :type => 'thumbnail', :create_thumbnails => true, :tag => 'original,lowres')
+
+        { :item_id => item_id, :original_file_id => original_file_id, :lowres_file_id => lowres_file_id }
+      end
+
+      # SEQUENCE THAT CREATE AN ITEM AND THE PROXY USING THE FILE URI/PATH
+      def item_create_with_proxy_using_file_uri(args = { }, options = { })
+        original_file_uri = args[:original_file_uri] || args[:original]
+        file_type = args[:file_type] || 'video'
+
+        import_tag = args[:import_tag] || 'lowres'
+
+        placeholder_args = args[:placeholder_args] ||= { :container => 1, :video => 1 }
+
+        storage_id = args[:storage_id]
+
+        # /API/import/placeholder/?container=1&video=1
+        placeholder = import_placeholder(placeholder_args)
+        placeholder_item_id = placeholder['id']
+
+        # /API/import/placeholder/VX-99/video/?uri=file%3A%2F%2F%2Fsrv%2Fmedia1%2Ftest_orginal2.mp4&tag=lowres
+        item = import_placeholder_item(:item_id => placeholder_item_id, :type => file_type, :uri => original_file_uri, :tag => import_tag)
+        item_id = item['file'].first['id']
+
+        # /API/item/VX-99/shape?tag=lowres&fileId=VX-136
+        #item_shape_import(:item_id => item_id, :tag => nil, :file_id => nil)
+
+        # /API/storage/VX-2/file/?path=storages/test/test_orginal2.mp4
+        #storage_file_get(:storage_id => storage_id)
       end
 
       def collection_get_by_name(args = { }, options = { })
