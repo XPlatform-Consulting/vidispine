54e7daf56bcdfe3bf0d059786f09077ebc02c2be
John Whitson <john.whitson@gmail.com>
2015-07-07 09:49:44 -0600

    Added import side car request

--- lib/vidispine/api/client/requests.rb
+++ lib/vidispine/api/client/requests.rb
@@ -2,6 +2,7 @@ require 'vidispine/api/client/requests/base_request'
 
 require 'vidispine/api/client/requests/import_placeholder'
 require 'vidispine/api/client/requests/import_placeholder_item'
+require 'vidispine/api/client/requests/import_sidecar_file'
 require 'vidispine/api/client/requests/import_using_uri'
 
 require 'vidispine/api/client/requests/item_delete'
